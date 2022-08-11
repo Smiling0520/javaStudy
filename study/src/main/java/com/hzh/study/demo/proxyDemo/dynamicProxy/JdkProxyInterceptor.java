fc8f932faac34aeddf3826a6617f5cacb60387f8
hua <984858159@qq.com>
2022-08-11 23:44:34 +0800

    JDK动态代理模式

--- study/src/main/java/com/hzh/study/demo/proxyDemo/dynamicProxy/JdkProxyInterceptor.java
+++ study/src/main/java/com/hzh/study/demo/proxyDemo/dynamicProxy/JdkProxyInterceptor.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Method;
 /**
  * @Author huzhenhua
  * @CreateTime 2022-08-11 23:07:25
- * @Description
+ * @Description 在 Java 动态代理机制中 InvocationHandler 接口和 Proxy 类是核心。
  */
 public class JdkProxyInterceptor implements InvocationHandler {
 
@@ -16,6 +16,16 @@ public class JdkProxyInterceptor implements InvocationHandler {
         this.target = target;
     }
 
+    /*
+     * @description:你通过Proxy 类的 newProxyInstance() 创建的代理对象在调用方法的时候，
+     * 实际会调用到实现InvocationHandler 接口的类的 invoke()方法。 你可以在 invoke() 方法中自定义处理逻辑，比如在方法执行前后做什么事情
+     * @author: huzhenhua
+     * @date: 2022/8/11 23:40 
+     * @param proxy 动态生成的代理类
+     * @param method 与代理类对象调用的方法相对应
+     * @param args 当前 method 方法的参数
+     * @return: java.lang.Object
+     **/
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         System.out.println("before send()");
--- study/src/main/java/com/hzh/study/demo/proxyDemo/dynamicProxy/ProxyFactory.java
+++ study/src/main/java/com/hzh/study/demo/proxyDemo/dynamicProxy/ProxyFactory.java
@@ -9,6 +9,9 @@ import java.lang.reflect.Proxy;
  */
 public class ProxyFactory {
 
+    // 类加载器，用于加载代理对象。
+    // 被代理类实现的一些接口；
+    // 实现了 InvocationHandler 接口的对象
     public static Object getProxy(Object target) {
         return Proxy.newProxyInstance(target.getClass().getClassLoader(),
                 target.getClass().getInterfaces(), new JdkProxyInterceptor(target));
